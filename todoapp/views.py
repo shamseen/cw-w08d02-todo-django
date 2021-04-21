c2ef112d157ac144b7baa2b6a93a913c333bca1a
shamseen <shamseen.c@gmail.com>
2021-04-21 11:33:49 -0400

    startapp + runserver => django crud GUI

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from .models import Todo
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import TodoSerializer
 
-# Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+    ## The Main Query for the index route
+    queryset = Todo.objects.all()
+
+    # The serializer class for serializing output
+    serializer_class = TodoSerializer
+    
+    # optional permission class set permission level
+    permission_classes = [permissions.AllowAny] #Coule be [permissions.IsAuthenticated]
